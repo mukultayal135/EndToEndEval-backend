62062fe5d4ead277c33ef97237d81c6c2e432457
mukultayal135 <mukultayal135@gmail.com>
2023-03-10 00:02:09 +0530

    feat: add cors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const contentRouter = require('./src/routes/contentRouter');
 const entryRouter = require('./src/routes/entryRouter');
+
 const tokenValidation = require('./src/middlewares/tokenValidation');
 
 const app = express();
 const port = 8000;
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 
 app.use(tokenValidation.tokenValidation);
 app.use(express.json());
